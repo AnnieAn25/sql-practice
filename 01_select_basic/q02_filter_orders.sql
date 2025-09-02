910d2367a74c6f9aaf3aa554e3458c4255e7f063
Annie <qiaozhi.an@gmail.com>
2025-09-02 13:10:16 -0400

    Update q02_filter_orders.sql

--- 01_select_basic/q02_filter_orders.sql
+++ 01_select_basic/q02_filter_orders.sql
@@ -1,1 +1,5 @@
 add q02_filter_orders.sql
+-- Problem: Find all orders placed after 2021-01-01
+--Goal: Practice WHERE + ORDER BY
+--Expected Output: List of orders with order_id, customer_id, order_date, sorted by date
+
