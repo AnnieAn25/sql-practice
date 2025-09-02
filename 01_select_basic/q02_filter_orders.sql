e492b63193408d9b75ca2ffdeb4421a19436eb66
Annie <qiaozhi.an@gmail.com>
2025-09-02 13:12:49 -0400

    Update q02_filter_orders.sql

--- 01_select_basic/q02_filter_orders.sql
+++ 01_select_basic/q02_filter_orders.sql
@@ -3,3 +3,7 @@ add q02_filter_orders.sql
 --Goal: Practice WHERE + ORDER BY
 --Expected Output: List of orders with order_id, customer_id, order_date, sorted by date
 
+SELECT order_id, customer_id, order_date
+FROM Orders
+WHERE order_date > '2021-01-01'
+ORDER BY order_date ASC;
