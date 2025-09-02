c03d762cd6ebbc9e6fc37be56a964c98cd6c4849
Annie <qiaozhi.an@gmail.com>
2025-09-02 13:08:26 -0400

    Update q01_select_customers.sql

--- 01_select_basic/q01_select_customers.sql
+++ 01_select_basic/q01_select_customers.sql
@@ -1,1 +1,6 @@
 add q01_select_customers.sql
+-- Problem: Select all customers from the Customers table
+-- Goal:Understand basic SELECT * query
+-- Expected Output: Full list of cusotmers with all colums
+SELECT *
+FROM Customers;
