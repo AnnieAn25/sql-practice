e7642d4192e1d12f0927c00b9a411017e39d11ad
Annie <qiaozhi.an@gmail.com>
2025-09-02 21:57:16 -0400

    Update q01_inner_join_customers_orders.sql

--- 03_joins/q01_inner_join_customers_orders.sql
+++ 03_joins/q01_inner_join_customers_orders.sql
@@ -1,1 +1,6 @@
 add q01_inner_join_customers_orders.sql
+-- match every order with the customer name
+SELECT o.order_id, c.customer_name, o.order_date
+FROM Orders O
+INNER JOIN Customer C
+  ON o.customer_id = c.customer_id;
