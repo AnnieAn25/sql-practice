561c0549ac2911d37b8f1fdd67fe615885798c76
Annie <qiaozhi.an@gmail.com>
2025-09-02 22:04:32 -0400

    Update q02_left_join_products_orders.sql

--- 03_joins/q02_left_join_products_orders.sql
+++ 03_joins/q02_left_join_products_orders.sql
@@ -1,1 +1,6 @@
 add q02_left_join_products_orders.sql
+--return all products order history
+SELECT p.product_id, p.product_name, o.order_id
+FROM product p
+LEFT JOIN order o
+  ON   p.product_id = o.product_id;
