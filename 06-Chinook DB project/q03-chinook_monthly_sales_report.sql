5d88cfcecaaf4b2de803f273722e61873a76b8fe
Annie <qiaozhi.an@gmail.com>
2025-09-03 22:26:13 -0400

    Delete 06-Chinook DB project directory

--- 06-Chinook DB project/q03-chinook_monthly_sales_report.sql
+++
@@ -1,6 +0,0 @@
-add q03-chinook_monthly_sales_report.sql
--- return monthly sales
-SELECT STRFTIME('%Y-%m', InvoiceDate) AS month, SUM(Total) AS sales
-FROM Invoice 
-GROUP BY month
-ORDER BY month;
